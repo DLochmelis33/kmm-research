2401210b04452c7eadb5a8b2b179c8a93e64f828
Denis Lochmelis <dlochmelis@gmail.com>
2023-02-22 23:13:08 +0100

    Improve setup.sh

--- kotlin-native/litmus/src/nativeInterop/setup.sh
+++ kotlin-native/litmus/src/nativeInterop/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mkdir /usr/include/kaffinity
-gcc kaffinity_gnu.c -c -o /usr/include/kaffinity/kaffinity_gnu.o
+PATH_TO_LIB_BINARY=/usr/include/kaffinity/kaffinity_gnu.o
+
+gcc kaffinity_gnu.c -c -o $PATH_TO_LIB_BINARY
+echo "linkerOpts.linux = $PATH_TO_LIB_BINARY" > kaffinity.def
