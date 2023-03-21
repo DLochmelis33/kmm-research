a30235ce538e070ddd60c5e4a9b7a6e9b70fa006
Denis Lochmelis <dlochmelis@gmail.com>
2023-03-21 13:22:50 +0100

    Add chi-squared distribution test

--- kotlin-native/litmus/src/commonMain/kotlin/Main.kt
+++ kotlin-native/litmus/src/commonMain/kotlin/Main.kt
@@ -1,23 +1,49 @@
 import kotlin.time.Duration.Companion.seconds
 
 fun main() {
-    val runner = WorkerTestRunner
+//    val sample1 = listOf(10000, 20000, 30000)
+//    val sample2 = listOf(5000, 10000, 15000)
+//    println(chiSquaredTest(sample1, sample2))
+    distributionTest()
+}
 
+fun distributionTest() {
+    val runner = WorkerTestRunner
     val parameters = variateParameters(
-            getAffinityManager()?.scheduleUnrestricted2().orStub2(),
+            getAffinityManager()?.scheduleShort2().orStub2(),
             generateSequence(2) { it * 2 }.take(8).toList(),
             listOf(null /* { MemShuffler(50_000) } */),
     ).toList()
-    val singleTestDuration = 3.seconds
-    println("parameters count: ${parameters.size}")
-    println("ETA: T+ ${(singleTestDuration * parameters.size).toComponents { m, s, _ -> "$m m $s s" }}")
-    var cnt = 1
-    val results = parameters.map { param ->
-        val result = runner.runTest(singleTestDuration, param, ::KindaMP)
-        println("done ${cnt++} / ${parameters.size}")
-        result
+
+    for (testCount in generateSequence(1000) { it * 2 }.take(10)) {
+        println("test count: $testCount...")
+
+        val results = List(10) {
+            parameters.map { param ->
+                runner.runTest(testCount, param, ::JCS07_SB)
+            }.flatten().merge()
+        }
+
+        outer@for (r1 in results) {
+            for (r2 in results) {
+                if (r1 == r2) continue
+
+                // ensure same order
+                val sample1 = r1.map { it.count }
+                val sample2 = r1.map { o -> r2.firstOrNull { it.outcome == o.outcome }?.count ?: 0 }
+
+                val accept = chiSquaredTest(sample1, sample2)
+                if(!accept) {
+                    println("rejected!")
+                    r1.prettyPrint()
+                    r2.prettyPrint()
+                    break@outer
+                }
+            }
+        }
     }
-    println("in total:")
-    val total = results.flatten().merge()
-    total.prettyPrint()
 }
+
+//            val singleTestDuration = 3.seconds
+//            println("parameters count: ${parameters.size}")
+//            println("ETA: T+ ${(singleTestDuration * parameters.size).toComponents { m, s, _ -> "$m m $s s" }}")
--- kotlin-native/litmus/src/commonMain/kotlin/Util.kt
+++ kotlin-native/litmus/src/commonMain/kotlin/Util.kt
@@ -26,3 +26,43 @@ fun Int.pow(power: Int): Int {
     repeat(power) { result *= this@pow }
     return result
 }
+
+// returns true if samples have the same distribution with 0.05 significance level
+// written almost completely by ChatGPT :)
+fun chiSquaredTest(sample1: List<Int>, sample2: List<Int>): Boolean {
+    require(sample1.size == sample2.size) { "incomparable samples" }
+    val n = sample1.size // number of categories
+    val dof = n - 1 // degrees of freedom
+    val obs = Array(2) { IntArray(n) } // observed frequencies
+
+    // fill in observed frequencies
+    for (i in 0 until n) {
+        obs[0][i] = sample1[i]
+        obs[1][i] = sample2[i]
+    }
+
+    // calculate expected frequencies
+    val total1 = sample1.sum()
+    val total2 = sample2.sum()
+    val exp = Array(2) { DoubleArray(n) }
+    for (i in 0 until n) {
+        exp[0][i] = total1.toDouble() * (sample1[i] + sample2[i]) / (total1 + total2)
+        exp[1][i] = total2.toDouble() * (sample1[i] + sample2[i]) / (total1 + total2)
+    }
+
+    // calculate chi-squared test statistic
+    var chiSq = 0.0
+    for (i in 0 until n) {
+        for (j in 0 until 2) {
+            val o = obs[j][i].toDouble()
+            val e = exp[j][i]
+            chiSq += (o - e) * (o - e) / e
+        }
+    }
+
+    // chi-squared cdf with significance = 0.05
+    val chiSquaredDist = listOf(0.0, 3.841, 5.991, 7.815, 9.488, 11.070)
+    val targetChiSq = chiSquaredDist[dof]
+
+    return chiSq < targetChiSq
+}
