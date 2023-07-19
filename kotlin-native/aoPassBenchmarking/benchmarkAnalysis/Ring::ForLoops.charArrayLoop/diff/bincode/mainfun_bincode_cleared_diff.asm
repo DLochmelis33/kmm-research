3a5cf691da2a11674810511290123c2aa0b763c5
GlebSolovev <lionyoung@mail.ru>
2023-07-19 03:16:56 +0200

    Extract tests into Main-s, compile artifacts

--- kotlin-native/aoPassBenchmarking/benchmarkAnalysis/Ring::ForLoops.charArrayLoop/diff/bincode/mainfun_bincode_cleared_diff.asm
+++
@@ -1,3 +0,0 @@
---- ../../benchmarkAnalysis/Ring::ForLoops.charArrayLoop/baseline/bincode/mainfun_bincode.asm	2023-07-18 05:45:44.564696442 +0200
-+++ ../../benchmarkAnalysis/Ring::ForLoops.charArrayLoop/unordered/bincode/mainfun_bincode.asm	2023-07-18 05:49:58.944627187 +0200
-
--- kotlin-native/aoPassBenchmarking/benchmarkAnalysis/Ring::ForLoops.uLongArrayLoop/diff/bincode/mainfun_bincode_cleared_diff.asm
+++
@@ -1,3 +0,0 @@
---- ../../benchmarkAnalysis/Ring::ForLoops.uLongArrayLoop/baseline/bincode/mainfun_bincode.asm	2023-07-18 05:45:53.384694342 +0200
-+++ ../../benchmarkAnalysis/Ring::ForLoops.uLongArrayLoop/unordered/bincode/mainfun_bincode.asm	2023-07-18 05:50:07.314621798 +0200
-
